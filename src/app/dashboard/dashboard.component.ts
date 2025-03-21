8accf2be4c64d8c3bd5847ac8230662b171f29b9
dethenrik <almand1999@hotmail.com>
2025-03-21 09:54:44 +0100

    asd

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../services/firebase.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  isLoggedIn = false;
+  isAdmin = false;
+  games = [
+    { id: '1', title: 'Spil 1', imageUrl: 'https://via.placeholder.com/200' },
+    { id: '2', title: 'Spil 2', imageUrl: 'https://via.placeholder.com/200' },
+    { id: '3', title: 'Spil 3', imageUrl: 'https://via.placeholder.com/200' }
+  ];
+  constructor(private firebaseService: FirebaseService) {}
+  ngOnInit(): void {
+    this.checkLoginStatus();
+  }
+  async checkLoginStatus() {
+    const user = this.firebaseService.getCurrentUser();
+    this.isLoggedIn = !!user;  // If user exists, set isLoggedIn to true
 
+    if (this.isLoggedIn && user) {
+      this.isAdmin = await this.firebaseService.checkIfAdmin(user.uid);
+    }
+  }
 }
